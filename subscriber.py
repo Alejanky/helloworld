f092fed2b6b81b2e770a56a3490ae4884f0d3564
Alejanky <alejandr_venegas@hotmail.com>
2016-04-26 23:44:12 +0000

    oi
    Signed-off-by: Alejanky <alejandr_venegas@hotmail.com>

---
+++ subscriber.py
@@ -0,0 +1,12 @@
+import paho.mqtt.client as mqtt
+def on_connect(client, userdate, flags, rc):
+	print('Connected with result code '+str(rc))
+	client.subscribe('poi')
+def on_message(client, userdate, msg):
+	print(msg.topic+' '+str(msg.payload))
+client=mqtt.Client()
+client.on_connect=on_connect
+client.on_message=on_message
+client.connect('test.mosquitto.org',1883,60)
+
+client.loop_forever()
