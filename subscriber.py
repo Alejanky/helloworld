b801440f8978317702439b03b06c0e5641de36f7
lepoi <terminallyquack@gmail.com>
2016-04-26 17:07:23 -0700

    argh

--- subscriber.py
+++ subscriber.py
@@ -4,7 +4,7 @@ def on_connect(client, userdate, flags, rc):
 	client.subscribe('poi')
 def on_message(client, userdate, msg):
 	print(msg.topic+' '+str(msg.payload))
-	will_set('poi', 'Hola', 0, False)
+	publish('poi', 'Hola')
 
 
 
